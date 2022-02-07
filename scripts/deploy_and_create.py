b7a12b575a20b47ff120f54f9307062350c725ba
Brian Neeland <brian.neeland@protonmail.com>
2022-02-07 07:57:56 -0700

    Picture 1 deployed to opensea

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,7 +1,16 @@
-from scripts.helpful_scripts import get_account, SimpleCollectible
+from brownie import SimpleCollectible
+from scripts.helpful_scripts import get_account
 
-token_uri = ''
+token_uri = 'https://ipfs.io/ipfs/QmYcBkgTi8Zta8xCkMjrrycDr5RrR3Qs4h4fE3pGBQAq3N?filename=1.json'
+OPENSEA_URL = 'https://testnets.opensea.io/assets/{}/{}'
+
+def deploy_and_create():
+    account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    tx = simple_collectible.createCollectible(token_uri, {"from": account})
+    tx.wait(1)
+    print(f"NFT deployed at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
+    print("Wait up to 20 minutes and hit refresh metadata button")
 
 def main():
-    account = get_account()
-    simple_collective = SimpleCollectible.deploy({"from": account})
+    deploy_and_create()
